ca6c4c8d8ef4e30467598ac2956106afe4991af1
Rifat <mrifat.hammad@gmail.com>
2013-05-22 15:28:46 +0300

    car_rental

---
+++ car_rental/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarRental::Application.config.secret_key_base = '15a7871130e7fccc85e482f9e6674d611f522674eb78a3f9f8fbe70a1800343dc84cd64d36925d563e356ef35abfbcbf2e9f5a12520fb1ed54436a69fd5e03f1'
